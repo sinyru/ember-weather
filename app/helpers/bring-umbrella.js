c065d2c41e27fcfe1ea43266dbf5fa07401c3ab7
“Zhu Chen” <znchen720@gmail.com>
2017-03-29 19:19:55 -0400

    Add two images to the background
    Added two images of weather logo and styled them.
    Styled the "hunhun" word

--- app/helpers/bring-umbrella.js
+++ app/helpers/bring-umbrella.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function bringUmbrella(params) {
-  if (params.toString()==="Rain"){
+  if (params.toString()==="Rain" || params.toString()==="Thunderstorm"){
     return true;
   } else {
     return false;
