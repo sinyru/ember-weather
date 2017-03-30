849635446b0b694b1e620ac5701302f3867214bb
“Zhu Chen” <znchen720@gmail.com>
2017-03-30 19:39:10 -0400

    Fix bug when user create apparel
    Fixed the issue of user creating new apparel with no name input.
    Fixed the issue of user able to go into the apparels/apparel url
    without logging in.

--- app/routes/apparel.js
+++ app/routes/apparel.js
@@ -4,25 +4,34 @@ import RSVP from 'rsvp';
 export default Ember.Route.extend({
   ajax: Ember.inject.service(),
   model () {
-    return RSVP.Promise.resolve({});
+
+    return this.get('store').findAll('apparel')
+    .then(()=>RSVP.Promise.resolve({}))
+    ;
+
   },
   actions: {
     create(apparel) {
-      return this.get('ajax').request('/apparels', {
-        method: 'POST',
-        data: {
-          apparel: {
-            name: apparel.name,
-            clothing_type: apparel.clothingType,
-          }
-        },
-      })
-      .then(()=>this.transitionTo('apparels'))
-      .catch(() => {
+      if(apparel.name!=="") {
+        return this.get('ajax').request('/apparels', {
+          method: 'POST',
+          data: {
+            apparel: {
+              name: apparel.name,
+              clothing_type: apparel.clothingType,
+            }
+          },
+        })
+        .then(()=>this.transitionTo('apparels'))
+        .catch(() => {
+          this.get('flashMessages')
+          .danger('There was a problem. Are you sure you fill in everything?');
+        })
+        ;
+      } else {
         this.get('flashMessages')
         .danger('There was a problem. Are you sure you fill in everything?');
-      })
-      ;
+      }
     }
   }
 });
